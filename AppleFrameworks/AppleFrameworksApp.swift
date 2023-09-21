6989bbb393c0cb27a3df6f22d2592d80f8c351a1
Vlad Polevoi <vlad.polewoi1@gmail.com>
2023-09-21 12:22:45 +0300

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Quest76 on 21.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
