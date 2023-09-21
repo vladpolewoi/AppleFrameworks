c8253a50687f3f95b0d7317b411005be8b33ac2a
Vlad Polevoi <vlad.polewoi1@gmail.com>
2023-09-21 15:34:10 +0300

    Init

---
+++ AppleFrameworks/UIKit Components/SafariView.swift
@@ -0,0 +1,21 @@
+//
+//  SafariVIew.swift
+//  AppleFrameworks
+//
+//  Created by Quest76 on 21.09.2023.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+        
+    }
+}
